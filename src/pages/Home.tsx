6012a358a770e91aba6934b96a862cdb1798e2b2
Jinnie <hjk7639@gmail.com>
2023-04-11 15:49:37 +0900

    [Style] Home 페이지 생성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,23 @@
 import SideInfo from '../components/SideInfo';
+import ItemButtons from '../components/ItemButton';
+import TermButtons from '../components/TermButton';
+import ItemLists from '../components/ItemList';
+import { Homelayout, Contentlayout, Buttonlayout } from '../style/Home.styled';
+
 const Home = () => {
-  return <SideInfo />;
+  return (
+    <Homelayout>
+      <Contentlayout>
+        <h1>Hi, JINNIE</h1>
+        <Buttonlayout>
+          <ItemButtons />
+          <TermButtons />
+        </Buttonlayout>
+        <ItemLists />
+      </Contentlayout>
+      <SideInfo />
+    </Homelayout>
+  );
 };
 
 export default Home;
---
+++ src/style/Home.styled.ts
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled';
+
+export const Homelayout = styled.section`
+  width: 980px;
+  display: flex;
+  justify-content: end;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+
+export const Contentlayout = styled.section`
+  width: 618px;
+  height: 680px;
+  padding-top: 20px;
+  padding-left: 22px;
+  border-left: 1px solid #000000;
+  h1 {
+    margin-bottom: 47px;
+    font-size: 48px;
+    font-weight: 400;
+  }
+`;
+
+export const Buttonlayout = styled.div`
+  display: flex;
+  align-items: end;
+  gap: 20px;
+`;
