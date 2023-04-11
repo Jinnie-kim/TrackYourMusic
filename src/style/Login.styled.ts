ba39a4c277e7fe72e5957288cc70d7db4317d346
Jinnie <hjk7639@gmail.com>
2023-04-11 22:41:12 +0900

    [Style] Login 페이지 로그인 버튼 태그 a 태그로 변경

--- src/style/Login.styled.ts
+++ src/style/Login.styled.ts
@@ -13,12 +13,13 @@ export const LoginTitle = styled.h1`
   font-size: 40px;
 `;
 
-export const LoginButton = styled.button`
+export const LoginButton = styled.a`
   width: 140px;
   height: 49px;
   margin: 0 auto;
   border-radius: 20px;
   font-size: 14px;
+  line-height: 49px;
   color: #ffffff;
   background-color: #1dda59;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
