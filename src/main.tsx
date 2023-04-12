b051bc728aabf3c04139c89a22fa5aeb3ca76dba
Jinnie <hjk7639@gmail.com>
2023-04-12 21:13:48 +0900

    [Feat] App 상단에 리덕스 Provider로 store를 전역에 공유

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { Global } from '@emotion/react';
 import App from './App';
+import store from './store/store';
 import { GlobalStyle } from './style/Global.styled';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <>
     <Global styles={GlobalStyle} />
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </>
+  // </React.StrictMode>
 );
