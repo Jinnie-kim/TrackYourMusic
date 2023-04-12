da1ab099413594348b88cd001cc4b56856686ff8
Jinnie <hjk7639@gmail.com>
2023-04-12 15:48:56 +0900

    [Feat] Loading 페이지에서 useAuth 훅으로 accessToken을 발급받은 후 home으로 이동

--- src/pages/Loading.tsx
+++ src/pages/Loading.tsx
@@ -1,6 +1,24 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
 import { Loadinglayout, LoadSpinner } from '../style/Loading.styled';
 
+const code = new URLSearchParams(window.location.search).get('code');
+
 const Loading = () => {
+  const result = useAuth(code!);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!result) return;
+
+    const loadTime = setTimeout(() => {
+      navigate('/home');
+    }, 2000);
+
+    return () => clearTimeout(loadTime);
+  }, [result]);
+
   return (
     <Loadinglayout>
       <h1>Login ...</h1>
