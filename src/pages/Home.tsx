72353f0db502bf1ad23ab98d06758fa2b8337b27
Jinnie <hjk7639@gmail.com>
2023-04-12 21:44:14 +0900

    [Feat] Home 페이지에서 새로고침 시 accessToken이 초기화 되는 에러 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,26 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { tokenAction } from '../store/token-slice';
 import SideInfo from '../components/SideInfo';
 import ItemButtons from '../components/ItemButton';
 import TermButtons from '../components/TermButton';
 import ItemLists from '../components/ItemList';
 import { Homelayout, Contentlayout, Buttonlayout } from '../style/Home.styled';
 
+interface tokenType {
+  token: { accessToken: string };
+}
+
 const Home = () => {
+  const dispatch = useDispatch();
+  const accessToken = useSelector((state: tokenType) => state.token.accessToken);
+
+  useEffect(() => {
+    if (!accessToken && localStorage.getItem('accessToken') !== null) {
+      dispatch(tokenAction.getToken(localStorage.getItem('accessToken')));
+    }
+  }, [accessToken]);
+
   return (
     <Homelayout>
       <Contentlayout>
