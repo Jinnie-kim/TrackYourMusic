7ae3cdbd714b074212a769b419fa29ac845a3b12
Jinnie <hjk7639@gmail.com>
2023-04-13 14:06:10 +0900

    [Feat] artist 버튼, track 버튼 선택에 따라 버튼 state에 맞는 데이터 정보 화면에 렌더

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,3 +1,4 @@
+import { useSelector } from 'react-redux';
 import { ItemListlayout, ItemList } from '../style/ItemList.styled';
 
 interface TopItemProp {
@@ -5,17 +6,34 @@ interface TopItemProp {
   topTracks: TopTracks[];
 }
 
+interface ButtonProp {
+  button: { isArtist: boolean; isTrack: boolean };
+}
+
 const ItemLists = ({ topArtists, topTracks }: TopItemProp) => {
+  const isArtist = useSelector((state: ButtonProp) => state.button.isArtist);
+  const isTrack = useSelector((state: ButtonProp) => state.button.isTrack);
+
   return (
     <ItemListlayout>
-      {topArtists.map((topArtist) => {
-        return (
-          <ItemList key={topArtist.id}>
-            <img src={topArtist.images[2].url} alt="artist image" />
-            <p>{topArtist.name}</p>
-          </ItemList>
-        );
-      })}
+      {isArtist &&
+        topArtists.map((topArtist) => {
+          return (
+            <ItemList key={topArtist.id}>
+              <img src={topArtist.images[2].url} alt="artist image" />
+              <p>{topArtist.name}</p>
+            </ItemList>
+          );
+        })}
+      {isTrack &&
+        topTracks.map((topTrack) => {
+          return (
+            <ItemList key={topTrack.id}>
+              <img src={topTrack.album.images[0].url} alt="artist image" />
+              <p>{topTrack.name}</p>
+            </ItemList>
+          );
+        })}
     </ItemListlayout>
   );
 };
