5897407b704c3537dd266524c06637e685c6f18f
Jinnie <hjk7639@gmail.com>
2023-04-13 14:00:02 +0900

    [Feat] artist 버튼, track 버튼을 클릭했을 때 클릭한 버튼의 id를 dispatch함수로 전역 state로 저장

--- src/components/ItemButton.tsx
+++ src/components/ItemButton.tsx
@@ -1,15 +1,21 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { ItemButtonBox, ItemButton } from '../style/Button.styled';
+import { buttonAction } from '../store/button-slice';
 
 const ItemButtons = () => {
   const [isArtists, setisArtists] = useState(true);
   const [isTracks, setisTracks] = useState(false);
 
+  const dispatch = useDispatch();
+
   const selectItemHandler = (e: React.MouseEvent) => {
     if (e.currentTarget.id === 'artist') {
+      dispatch(buttonAction.isArtist(true));
       setisArtists(true);
       setisTracks(false);
     } else if (e.currentTarget.id === 'track') {
+      dispatch(buttonAction.isTrack(true));
       setisTracks(true);
       setisArtists(false);
     }
