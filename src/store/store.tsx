ff10b2482c52a51956e02ec57b44bf9d200c4e1b
Jinnie <hjk7639@gmail.com>
2023-04-13 13:55:46 +0900

    [Feat] button slice를 store에 등록

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import tokenSlice from './token-slice';
+import buttonSlice from './button-slice';
 
 const store = configureStore({
-  reducer: { token: tokenSlice.reducer },
+  reducer: { token: tokenSlice.reducer, button: buttonSlice.reducer },
 });
 
 export default store;
